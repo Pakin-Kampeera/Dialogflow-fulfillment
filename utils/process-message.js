b56ebe431c7acfaa39e53788634fb4404fbd33f9
Pakin Kampeera <armpakin@hotmail.com>
2021-06-14 20:02:55 +0700

    Modify: facebook dependency

--- utils/process-message.js
+++ utils/process-message.js
@@ -1,4 +1,4 @@
-const fetch = require('node-fetch');
+const FBMessenger = require('fb-messenger');
 
 const projectID = 'fbmessengerbot-ulur';
 const sessionID = '123456';
@@ -17,11 +17,17 @@ const sessionPath = sessionClient.sessionPath(projectID, sessionID);
 const FB_ACCESS_TOKEN = process.env.FACEBOOK_ACCESS_TOKEN;
 
 const sendTextMessage = (userID, text) => {
-  return fetch(`https://graph.facebook.com/v10.0/me/messages?access_token=${FB_ACCESS_TOKEN}`, {
-    headers: { 'Content-Type': 'application/json' },
-    method: 'POST',
-    body: JSON.stringify({ messaging_type: 'RESPONSE', recipient: { id: userID }, message: { text } }),
-  });
+  const messenger = new FBMessenger({ token: FB_ACCESS_TOKEN });
+  if (text.intent.displayName === 'Meme') {
+    const payloadFields = text.fulfillmentMessages[0].payload.fields;
+    messenger.sendTextMessage({ id: userID, text: payloadFields.text.stringValue });
+    messenger.sendImageMessage({
+      id: userID,
+      url: payloadFields.url.stringValue,
+    });
+  } else {
+    messenger.sendTextMessage({ id: userID, text: text.fulfillmentText });
+  }
 };
 
 module.exports = (event) => {
@@ -42,7 +48,7 @@ module.exports = (event) => {
     .detectIntent(request)
     .then((response) => {
       const result = response[0].queryResult;
-      return sendTextMessage(userID, result.fulfillmentText);
+      return sendTextMessage(userID, result);
     })
     .catch((err) => {
       console.log('ERROR', err);
